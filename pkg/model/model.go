b338cbf5f9451ac8b785fb0edf2e2cf92878702e
wuyan94zl <466720682@qq.com>
2020-12-18 15:41:55 +0800

    model public func

--- pkg/model/model.go
+++ pkg/model/model.go
@@ -2,49 +2,54 @@ package model
 
 import (
 	"github.com/wuyan94zl/api/pkg/database"
+	"gorm.io/gorm"
 )
+// 构建relationship查询
+func setRelationship(rom *gorm.DB, relationship []string) *gorm.DB {
+	for _, v := range relationship {
+		rom = rom.Preload(v)
+	}
+	return rom
+}
 
-// 创建一条数据
-func Create(model interface{}){
+// 创建数据
+func Create(model interface{}) {
 	database.DB.Create(model)
 }
 
 /** start查询相关 */
 
 // 主键获取一条数据
-func First(model interface{},id interface{},relationship ...string){
+func First(model interface{}, id interface{}, relationship ...string) {
 	rom := database.DB
-	for _,v := range relationship{
-		rom = rom.Preload(v)
-	}
-	rom.First(model,id)
+	rom = setRelationship(rom, relationship)
+	rom.First(model, id)
 }
 
 // 条件获取一条数据
-func GetOne(model interface{},condition []Condition,relationship ...string){
+func GetOne(model interface{}, condition []Condition, relationship ...string) {
 	rom := orm(condition)
-	for _,v := range relationship{
-		rom = rom.Preload(v)
-	}
+	rom = setRelationship(rom, relationship)
 	rom.First(model)
 }
 
-// 获取多条数据
-func GetAll(model interface{},condition []Condition,limit ...int){
+// 获取limit部分数据
+func GetLimit(model interface{}, condition []Condition, limit int, offset int, relationship ...string) {
 	rom := orm(condition)
-	if limit != nil{
-		switch len(limit) {
-		case 1:
-			rom = rom.Limit(limit[0])
-		case 2:
-			rom = rom.Offset(limit[1]).Limit(limit[0])
-		}
-	}
+	rom = rom.Offset(offset).Limit(limit)
+	rom = setRelationship(rom, relationship)
+	rom.Find(model)
+}
+
+// 获取所有数据
+func GetAll(model interface{}, condition []Condition, relationship ...string) {
+	rom := orm(condition)
+	rom = setRelationship(rom, relationship)
 	rom.Find(model)
 }
 
 // 单表分页查询
-func Paginate(model interface{}, pageInfo PageInfo, condition []Condition,relationship ...string) PageList {
+func Paginate(model interface{}, pageInfo PageInfo, condition []Condition, relationship ...string) PageList {
 	offset := (pageInfo.Page - 1) * pageInfo.PageSize
 	limit := pageInfo.PageSize
 	rom := orm(condition)
@@ -52,9 +57,7 @@ func Paginate(model interface{}, pageInfo PageInfo, condition []Condition,relati
 	rom.Model(model).Count(&count)
 	if count > 0 {
 		rom = rom.Offset(int(offset)).Limit(int(limit))
-		for _,v := range relationship{
-			rom = rom.Preload(v)
-		}
+		rom = setRelationship(rom, relationship)
 		rom.Find(model)
 	}
 	lastPage := (count / pageInfo.PageSize) + 1
@@ -64,27 +67,27 @@ func Paginate(model interface{}, pageInfo PageInfo, condition []Condition,relati
 /** end查询相关 */
 
 // 根据条件删除数据
-func Delete(model interface{},condition []Condition) bool{
+func Delete(model interface{}, condition []Condition) bool {
 	rom := orm(condition)
 	rom.Delete(model)
 	return true
 }
 
 // 删除当前模型数据
-func DeleteOne(model interface{}) bool{
+func DeleteOne(model interface{}) bool {
 	database.DB.Delete(model)
 	return true
 }
 
 // 删除当前模型数据
-func DeleteById(model interface{},Id interface{}){
-	database.DB.Delete(model,Id)
+func DeleteById(model interface{}, Id interface{}) {
+	database.DB.Delete(model, Id)
 }
 
 /**
 更新当前模型数据
- */
-func UpdateOne(model interface{}) interface{}{
+*/
+func UpdateOne(model interface{}) interface{} {
 	database.DB.Save(model)
 	return model
 }
